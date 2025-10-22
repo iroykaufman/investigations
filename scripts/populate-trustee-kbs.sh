e2e51332b9ceff12d14b3c1900ea566b2a8083c1
Roy Kaufman <rkaufman@redhat.com>
2025-10-22 15:27:22 +0300

    clevis-pin-trustee for attestation.
    Also, use the shared Containerfile for the custom image.
    
    Signed-off-by: Roy Kaufman <rkaufman@redhat.com>

--- scripts/populate-trustee-kbs.sh
+++ scripts/populate-trustee-kbs.sh
@@ -3,19 +3,22 @@
 set -euo pipefail
 # set -x
 
-if [[ "${#}" -ne 1 ]]; then
+if [[ "${#}" > 3 ]]; then
 	echo "Usage: $0 <path-to-ssh-public-key>"
+	echo "Optional: $0 <path-to-ssh-public-key> <SERVER_IP> <HOSTNAME>"
 	exit 1
 fi
 
 KEY=$1
 TRUSTEE_PORT=8080
-
+IP=$2
+if [[ ${IP} == "" ]]; then 
 # Setup reference values, policies and secrets
 until IP="$(./scripts/get-ip.sh trustee)" && [ -n "$IP" ] && curl "http://${IP}:${TRUSTEE_PORT}" >/dev/null 2>&1; do
 	echo "Waiting for KBS to be available..."
 	sleep 1
 done
+fi
 until ssh core@$IP \
 	-i "${KEY%.*}" \
 	-o StrictHostKeyChecking=no \
@@ -29,10 +32,14 @@ done
 BUTANE=pin-trustee.bu
 IGNITION="${BUTANE%.bu}.ign"
 
-sed "s/<IP>/$IP/" configs/remote-ign/${BUTANE} > tmp/${BUTANE}
+HOSTNAME=$3
+if [[ ${HOSTNAME} == "" ]]; then 
+HOSTNAME=${IP}
+fi
+sed "s/<IP>/$HOSTNAME/" configs/remote-ign/${BUTANE} > ${BUTANE}
 
 podman run --interactive --rm --security-opt label=disable \
-	--volume "$(pwd)/tmp:/pwd" \
+	--volume "$(pwd):/pwd" \
 	--workdir /pwd \
 	quay.io/confidential-clusters/butane:clevis-pin-trustee \
 	--pretty --strict /pwd/$BUTANE --output "/pwd/$IGNITION"
@@ -40,7 +47,7 @@ podman run --interactive --rm --security-opt label=disable \
 scp -i "${KEY%.*}" \
 	-o StrictHostKeyChecking=no \
 	-o UserKnownHostsFile=/dev/null \
-	tmp/${IGNITION} core@$IP:
+	/${IGNITION} core@$IP:
 
 ssh core@$IP \
 	-i "${KEY%.*}" \
--- trustee-on-GCP/scripts/upload_image_gcp.sh
+++ trustee-on-GCP/scripts/upload_image_gcp.sh
@@ -7,11 +7,11 @@ IMG_NAME=$2
 ## create a bucket to store the image
 gsutil mb gs://${BUCKET_NAME}
 
-gsutil cp ./${IMG_NAME}.tar-gcp.x86_64.tar.gz gs://${BUCKET_NAME}/
+gsutil cp ./coreos/${IMG_NAME}.x86_64.tar.gz gs://${BUCKET_NAME}/
 
 ## create the image in GCP
 gcloud compute images create ${IMG_NAME} \
-    --source-uri gs://${BUCKET_NAME}/${IMG_NAME}.tar-gcp.x86_64.tar.gz \
+    --source-uri gs://${BUCKET_NAME}/${IMG_NAME}.x86_64.tar.gz \
     --description "My custom ${IMG_NAME} image" \
     --guest-os-features UEFI_COMPATIBLE,GVNIC,VIRTIO_SCSI_MULTIQUEUE,SEV_SNP_CAPABLE,TDX_CAPABLE 
 
